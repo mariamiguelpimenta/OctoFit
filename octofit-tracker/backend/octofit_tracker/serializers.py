9dead5779c733bd5f93b49f1b1d0bef65eb6efc8
mariamiguelpimenta <maria.miguel.pimenta@accenture.com>
2026-03-04 17:35:44 +0000

    Implement initial Django app structure with models, views, serializers, and settings

---
+++ octofit-tracker/backend/octofit_tracker/serializers.py
@@ -0,0 +1,32 @@
+from rest_framework import serializers
+from .models import User, Team, Activity, Leaderboard, Workout
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = '__all__'
+
+    def to_representation(self, instance):
+        ret = super().to_representation(instance)
+        ret['id'] = str(ret['id'])
+        return ret
+
+class TeamSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Team
+        fields = '__all__'
+
+class ActivitySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Activity
+        fields = '__all__'
+
+class LeaderboardSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Leaderboard
+        fields = '__all__'
+
+class WorkoutSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Workout
+        fields = '__all__'
